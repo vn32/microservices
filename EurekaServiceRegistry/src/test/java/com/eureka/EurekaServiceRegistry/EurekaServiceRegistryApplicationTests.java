a017a1991d89d871a512012e82fdfa3e0127aee1
vn1012 <nishadvikas87@gmai.com>
2025-04-18 22:08:21 +0530

    Added eureka service registry

---
+++ EurekaServiceRegistry/src/test/java/com/eureka/EurekaServiceRegistry/EurekaServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eureka.EurekaServiceRegistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ hotelServices/src/test/java/com/hotelServices/hotelServices/HotelServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotelServices.hotelServices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
