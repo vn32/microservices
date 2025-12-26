6db92b577991d811951b86b47dd58c24bcdb61e6
Vikas Nishad <nishadvikas87@gmail.com>
2025-12-26 21:05:31 +0530

    final commit for config system

--- loans/src/main/java/com/vn32/loans/dto/ResponseDto.java
+++ loans/src/main/java/com/vn32/loans/dto/ResponseDto.java
@@ -3,12 +3,13 @@ package com.vn32.loans.dto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Schema(
         name = "Response",
         description = "Schema to hold successful response information"
 )
-@Data @AllArgsConstructor
+@Data @AllArgsConstructor @NoArgsConstructor
 public class ResponseDto {
 
     @Schema(
